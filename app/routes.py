b23f84d8b152ee0f200679eb01563ec9d6e84b25
MarianaU <mariana.urreav@uqvirtual.edu.co>
2025-02-22 16:52:22 -0500

    La base de datos esta conectada

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Blueprint, jsonify
 from app import db
-from flask import jsonify
 from app.models import Producto
 
-# Ruta de prueba
-def init_routes(app):
-    @app.route("/")
-    def home():
-        return "¡La base de datos está conectada!"
+bp = Blueprint("main", __name__)
 
-    @app.route("/productos")
-    def obtener_productos():
-        productos = Producto.query.all()
-        return jsonify([{"id": p.id, "nombre": p.nombre, "precio": p.precio} for p in productos])
+@bp.route("/")
+def home():
+    return "¡La base de datos está conectada!"
+
+@bp.route("/productos")
+def obtener_productos():
+    productos = Producto.query.all()
+    return jsonify([{"id": p.id, "nombre": p.nombre, "precio": p.precio} for p in productos])
