8b87644e7335cf301f4c2855033a36a9d4d6d806
MarianaU <mariana.urreav@uqvirtual.edu.co>
2025-02-22 16:04:57 -0500

    Primer commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    uri = os.getenv("DATABASE_URL")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)  # Reemplazar solo la primera ocurrencia
+
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
