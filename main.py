19597cbded4a0d44418f39c9d7c24c8ba9b20ba5
MarianaU <mariana.urreav@uqvirtual.edu.co>
2025-02-23 11:11:41 -0500

    Index con imagenes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 # Asegurar que Flask detecte la carpeta "templates"
-app = Flask(__name__, template_folder="app/templates")
+app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
