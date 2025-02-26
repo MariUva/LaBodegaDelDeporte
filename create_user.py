9ffeca02a6cf7d1aec1db9ab05b13679a8738b61
MarianaU <mariana.urreav@uqvirtual.edu.co>
2025-02-25 21:07:48 -0500

    Ensayo correo

---
+++ create_user.py
@@ -0,0 +1,21 @@
+from app import create_app, db
+from app.models import Usuario
+
+# Crear la aplicación
+app = create_app()
+
+# Establecer el contexto de la aplicación
+with app.app_context():
+    # Crear el usuario
+    usuario = Usuario(
+        correo="maraurrea1@gmail.com",
+        nombre="Lulu",
+        apellidos="Medrano"
+    )
+    usuario.set_password("1234")  # Hashear la contraseña
+
+    # Agregarlo a la base de datos
+    db.session.add(usuario)
+    db.session.commit()
+
+    print("Usuario creado exitosamente.")
