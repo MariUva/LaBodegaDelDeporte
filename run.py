5f28c2b4e96036e5d511fe5bf956cd8718e8d649
MarianaU <mariana.urreav@uqvirtual.edu.co>
2025-04-17 15:08:06 -0500

    Main and Run

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from app import create_app
-from waitress import serve
-import os
 
 """
 Este archivo es el encargado de ejecutar la aplicación, es decir, es el archivo principal de la aplicación.
@@ -8,6 +6,4 @@ Este archivo es el encargado de ejecutar la aplicación, es decir, es el archivo
 app = create_app()
 
 if __name__ == "__main__":
-    # Usar Waitress y el puerto dinámico proporcionado por Railway
-    port = int(os.getenv("PORT", 8000))  # Usa el puerto de Railway si está disponible
-    serve(app, host="0.0.0.0", port=port)  # Usar 0.0.0.0 para que la app sea accesible en todas las interfaces
+    app.run(debug=False)
