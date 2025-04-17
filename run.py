2dc784fd7612a7bb2f4cf19a49437bda1c40bab1
MarianaU <mariana.urreav@uqvirtual.edu.co>
2025-04-17 15:25:44 -0500

    Main and Run

---
+++ run.py
@@ -0,0 +1,9 @@
+from app import create_app
+
+"""
+Este archivo es el encargado de ejecutar la aplicación, es decir, es el archivo principal de la aplicación.
+"""
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
