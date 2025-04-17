9f82d03ecdb0e4fc3e4f0e38bbe772ce603e0ee8
MarianaU <mariana.urreav@uqvirtual.edu.co>
2025-04-17 15:41:42 -0500

    Cambios

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from app import create_app
+import os
 
-"""
-Este archivo es el encargado de ejecutar la aplicación, es decir, es el archivo principal de la aplicación.
-"""
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 8000))  # Usa el puerto de Railway o 8000 por defecto
+    app.run(host="0.0.0.0", port=port, debug=False)
